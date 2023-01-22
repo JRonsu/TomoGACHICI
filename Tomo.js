b05b2a221e2344a46c96e8270d612f07234fc0ab
Ronsou <samelsonjohn@protonmail.com>
2023-01-22 01:49:35 -0500

    Tomo User Input Name Added v2

--- Tomo.js
+++ Tomo.js
@@ -1,19 +1,29 @@
 // Create my TOMOGACHI
-//Evolution system based on level like pokmeon
+//Add Evolution system based on level like pokmeon
 //I want a trust system with anger and love
-//User inpupt their own name Tomo 
+
 
 class Tamagotchi {
-    constructor(tomoName) {
-      this.level = 1;
-      this.hunger = 0;
-      this.sleepyness = 0;
-      this.boredom = 0;
-      this.anger = 0;
-      this.love = 0;
+    // Define the Tamagotchi class
+    constructor(name) {
+        // Define the constructor function, which runs when a new Tamagotchi object is created
+        this.name = name; // Assign the input name to the 'name' property of the object
+        this.level = 1; // Assign level 1 to the object
+        this.hunger = 0; // Assign 0 to hunger
+        this.sleepyness = 0; // Assign 0 to sleepyness
+        this.boredom = 0; // Assign 0 to boredom
+        this.anger = 0; // Assign 0 to anger
+        this.love = 0; // Assign 0 to love
     }
 }
 
-let name = prompt("Enter a name for your Tamagotchi:"); // I dont like the interference of the prompt
-let myTamagotchi = new Tamagotchi(tomoName);
-console.log(`Your Tamagotchi's name is ${myTamagotchi.name}`);
+// Create our TomoGachi and name it
+function createTamagotchi() {
+    // Define the createTamagotchi function
+    let name = document.getElementById("name").value; // Get the value of the input 
+    let myTamagotchi = new Tamagotchi(name); // Create a new Tamagotchi object and assign the input name to it
+    console.log(`Your Tamagotchi's name is ${myTamagotchi.name}`); // Log the Tamagotchi's name to the console
+    document.getElementById("create-button").style.display = "none"; // make button disappear after 1 click
+    document.getElementById("name").style.display = "none"; // make input disappear after create button press
+    document.getElementById("label").style.display = "none";// make label message disappear after create button press
+}
