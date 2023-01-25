65c0d3f8f38d722131f71afacf3ceaf9ce9fe8c0
Ronsou <samelsonjohn@protonmail.com>
2023-01-25 14:24:20 -0500

    Add Feed Function

--- Tomo.js
+++ Tomo.js
@@ -1,7 +1,9 @@
 // Create my TOMOGACHI
 //Add Evolution system based on level like pokmeon
-//I want a trust system with anger and love
-
+// add cooldown to these buttons functions
+// make sleep a loading process -- make it block out all other functions and at intervals go up to 100 until button pressed
+// if nutrition level goes belo 10 5 times the Tomogotchi will run away  
+// add starting egg photo and switch to sprite after hatches
 
 
 class Tamagotchi {
@@ -10,9 +12,9 @@ class Tamagotchi {
         // Define the constructor function, which runs when a new Tamagotchi object is created
         this.name = name; // Assign the input name to the 'name' property of the object
         this.level = 1; // Assign level 1 to the object
-        this.hunger = 0; // Assign 0 to hunger
-        this.sleepyness = 0; // Assign 0 to sleepyness
-        this.boredom = 0; // Assign 0 to boredom
+        this.nutrition = 40; // Assign 0 to hunger
+        this.rest = 0; // Assign 0 to sleepyness
+        this.boredom = 50; // Assign 0 to boredom
         this.anger = 0; // Assign 0 to anger
         this.love = 0; // Assign 0 to love
     }
@@ -24,15 +26,29 @@ class Tamagotchi {
             this.hatch(); // check if egg can hatch
         }
     }
+    
     hatch() {
-        if (this.love >= 50) { // check if love is equal to or greater than 50
-            this.isHatched = true; // set hatch status to true
-            console.log(`Congratulations! Your Tamagotchi ${this.name} is hatched!`); // log message to console
-            document.getElementById("add-love-button").style.display = "none"; // hide the add love button
+        if (this.love >= 50) {
+            this.isHatched = true;
+            console.log(`Congratulations! Your Tamagotchi ${this.name} is hatched!`);
+            document.getElementById("add-love-button").style.display = "none";// when its hatched make button disappear
+            document.getElementById("feed-button").style.display = "block"; // when its hatched show feed button
         }
     }
+    
 
+    feed() {
+        if(this.nutrition > 0 && this.nutrition < 100) { // Check if nutrition is greater than 0 and less than 100
+            this.nutrition += 10; // Increment nutrition by 10
+            console.log("Tamagotchi is fed. Nutrition: " + this.nutrition);
+        } else if(this.nutrition >= 100) {
+            console.log("Tamagotchi is already full. Nutrition: " + this.nutrition);
+        } else {
+            console.log("Tamagotchi is hungry. Nutrition: " + this.nutrition);
+        }
+    }
 }
+
 
 let myTamagotchi;
 
@@ -46,16 +62,23 @@ function createTamagotchi() {
     document.getElementById("name").style.display = "none"; // make input disappear after create button press
     document.getElementById("label").style.display = "none";// make label message disappear after create button press
     gotchiLove();
+    
 }
 
+// Gotchi Love function & Hatch Egg Mechanic & TBC trust gain/lost mechanic
 function gotchiLove() {
     document.getElementById("add-love-button").style.display = "block"; // sets the love button to be visible
     document.getElementById("add-love-button").addEventListener("click", () => { //adds a click event listener to the love button
         myTamagotchi.addLove(); // calls the addLove function from the Tamagotchi class
+    });
+}
 
-        if (myTamagotchi.isHatched) { // checks if the Tamagotchi is hatched
-            document.getElementById("add-love-button").style.display = "none"; // if Tamagotchi is hatched, makes love button invisible
-        }
-        // I want to add a cd to the button 
+
+//Nutrition Function that effects love, sleepiness and anger and death if not fed within a certain amount of time
+function feed() {
+    document.getElementById("feed-button").style.display = "block"; // sets the love button to be visible
+    document.getElementById("feed-button").addEventListener("click", () => { //adds a click event listener to the feed button
+        myTamagotchi.feed(); // calls the feed function from the Tamagotchi class
+        // I want to add a cooldown to the button 
     });
 }
